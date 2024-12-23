3ec9a547de860cef3c56f9e7021a2c1ded44da81
nadav203 <nadav203@gmail.com>
2024-12-23 19:55:50 +0200

    Update engine.py
    addition + multiplication

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -6,3 +6,15 @@ class Value:
     def __repr__(self):
         return f"Value: data = {self.data}"
     
+    def __add__(self, other):
+        return Value(self.data + other.data)
+    
+    def __mul__(self, other):
+        return Value(self.data * other.data)
+    
+
+if __name__ == "__main__":
+    test_val = Value(5)
+    other_val = Value(6)
+    print(test_val * other_val)
+    
