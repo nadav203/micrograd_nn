4a388bb5757518c4f2e954a7e496c25aa68126bb
nadav203 <nadav203@gmail.com>
2024-12-23 20:00:44 +0200

    Update engine.py
    children + operation history init

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,20 +1,26 @@
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _operation=''):
         self.data = data
+        self._children = set(_children)
+        self._operation = _operation
     
     def __repr__(self):
         return f"Value: data = {self.data}"
     
     def __add__(self, other):
-        return Value(self.data + other.data)
+        return Value(self.data + other.data, (self, other), 'addition')
     
     def __mul__(self, other):
-        return Value(self.data * other.data)
+        return Value(self.data * other.data, (self, other), 'multiplication')
     
 
 if __name__ == "__main__":
     test_val = Value(5)
     other_val = Value(6)
-    print(test_val * other_val)
+    add = test_val + other_val
+    print(add)
+    print(add._children)
+    print(add._operation)
+
     
