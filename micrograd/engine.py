b1e98ae20af7497675607fa22407b6fa64138462
nadav203 <nadav203@gmail.com>
2024-12-24 17:10:29 +0200

    Update engine.py
    backward propagation add, multiply, tanh

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -7,19 +7,37 @@ class Value:
         self._children = set(_children)
         self._operation = _operation
         self.gradient = 0
+        self._backward = lambda: None
     
     def __repr__(self):
         return f"Value: data = {self.data}"
     
     def __add__(self, other):
-        return Value(self.data + other.data, _children=(self, other), _operation='+')
+        res = Value(self.data + other.data, _children=(self, other), _operation='+')
+        
+        def _backward():
+            self.gradient = 1.0 * res.gradient
+            other.gradient = 1.0 * res.gradient
+        res._backward = _backward
+        return res
     
     def __mul__(self, other):
-        return Value(self.data * other.data, _children=(self, other), _operation='*')
+        res = Value(self.data * other.data, _children=(self, other), _operation='*')
+
+        def _backward():
+            self.gradient = other.data * res.gradient
+            other.gradient = self.data * res.gradient
+        res._backward = _backward
+        return res
     
     def tanh(self):
         data = (math.exp(self.data * 2) - 1) / (math.exp(self.data * 2) + 1)
-        return Value(data, _children=(self, ), _operation='tanh')
+        res =  Value(data, _children=(self, ), _operation='tanh')
+
+        def _backward():
+            self.gradient = (1 - data**2) * res.gradient
+        res._backward = _backward
+        return res
 
     
 
@@ -27,11 +45,20 @@ if __name__ == "__main__":
     test_val = Value(5)
     other_val = Value(6)
     add = test_val + other_val
+    print('gradient: ', add._backward)
     print(add)
     print(add._children)
     print(add._operation)
     tanh = add.tanh()
     print(tanh)
     print(tanh._children)
+    tanh.gradient = 1
+    print('gradient:', add.gradient)
+    tanh._backward()
+    print('gradient:', add.gradient)
+    tanh._backward()
+    print('gradient:', add.gradient)
+    add._backward()
+    print('gradient:', test_val.gradient)
 
     
