4b5f3297857f83dd64819d5ee00193aa9eb6ca8a
nadav203 <nadav203@gmail.com>
2024-12-28 17:33:02 +0200

    neurnet.py
    Neuron function call

--- micrograd/neuralnet.py
+++ micrograd/neuralnet.py
@@ -1,4 +1,4 @@
-from micrograd.engine import Value
+from engine import Value
 import random
 
 class Neuron:
@@ -9,3 +9,14 @@ class Neuron:
         
         self.w = [value_gen() for _ in range(input_num)]
         self.bias = value_gen()
+    
+    def __call__(self, x):
+        # forward pass
+        activation = sum(w_i * x_i for w_i, x_i in zip(self.w, x)) + self.bias
+        res = activation.tanh()
+        return res
+    
+if __name__ == "__main__":
+    x = [2.0, 3.0]
+    n = Neuron(2)
+    print(n(x))
